090dc428694aa7914f042e1ce75a08b56335e7bc
NELLY1300 <lubangakenenelson0011@gmail.com>
2024-09-22 13:10:14 +0300

    Add files via upload

---
+++ multiples of 3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "multipes of 3 from 3 from 1 up to 20" << endl;
+    for (int i= 1; i<=20; i++) {
+    	if (i % 3 == 0) {
+    		cout <<i<<endl;
+		}
+	}
+
+    return 0;
+}
